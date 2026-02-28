187eed4f5ffce1d8bf97eb064dc1b3ca34f19801
Naheel <kknaheel16@gmail.com>
2026-02-28 17:39:32 +0530

    backend: init

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -4,5 +4,4 @@ from .quizzes import quizzes_bp
 from .recommendations import recommendations_bp
 from .progress import progress_bp
 from .admin import admin_bp
-# ASSESSMENT FEATURE DISABLED (faculty requirement)
-# from .assessments import assessments_bp
+from .assessments import assessments_bp
